f7b7e09f73150372c92dc0aeb6078db16e10b875
Gabbyb13 <Gabby@Batchbase.com.au>
2026-04-28 12:05:18 +0100

    fix: provide dummy key to stripe during build to prevent Vercel crash

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from 'stripe';
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || 'sk_test_dummy', {
   // https://github.com/stripe/stripe-node#configuration
   apiVersion: '2025-02-24.acacia' as any, // Updated to match valid Stripe API versions or removed completely
   appInfo: {
